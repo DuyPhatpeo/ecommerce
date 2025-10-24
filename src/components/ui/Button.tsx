0f861093e4a8101d16127b3407d702d366186df2
Tran Duy Phat <phattranduy00@gmail.com>
2025-10-24 07:52:03 +0700

    update code

--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -31,7 +31,7 @@ const Button: React.FC<ButtonProps> = ({
       type={type}
       {...props}
       className={`inline-flex items-center gap-2 ${justifyClass} 
-        transition-all duration-300 ${className}`}
+    transition-all duration-300 cursor-pointer ${className}`}
     >
       {iconPosition === "left" && icon}
       {label && <span>{label}</span>}
